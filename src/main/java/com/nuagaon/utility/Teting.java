c0541ecded60ed848ca1abe09bba388c4cd016b0
bikramjit <bikramjit.bardhan@mpayme.com>
2014-03-20 14:19:38 +0800

    This is Testing

---
+++ src/main/java/com/nuagaon/utility/Teting.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C) 2013 MPayMe Limited
+ * All rights reserved.
+ */
+package com.nuagaon.utility;
+
+/**
+ * @author bikramjit.bardhan
+ *
+ */
+public class Teting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git Testing");
+	}
+
+}
