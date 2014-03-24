98433215292b6082bd1738352dd1bda9105b8167
bikramjit <bikramjit.bardhan@mpayme.com>
2014-03-24 18:03:17 +0800

    Utility Commited-1

---
+++ src/main/java/com/nuagaon/utility/Utility.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2013 MPayMe Limited
+ * All rights reserved.
+ */
+package com.nuagaon.utility;
+
+/**
+ * @author bikramjit.bardhan
+ *
+ */
+public class Utility {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+}
