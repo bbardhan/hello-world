bef7eb6022f3949a6560391f6451135135898a06
bikramjit <bikramjit.bardhan@mpayme.com>
2014-03-25 10:20:30 +0800

    Revert "kesting"
    This reverts commit e07449e2727137e186bae0204196c465267732a7.

--- src/main/java/com/nuagaon/utility/Utility.java
+++ src/main/java/com/nuagaon/utility/Utility.java
@@ -14,7 +14,7 @@ public class Utility {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("this is utility kesting");
+		System.out.println("this is utility");
 
 	}
 
